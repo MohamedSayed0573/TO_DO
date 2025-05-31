69891aaf7c2c1f87ef43763c5deafd32bc525dac
Mohamed <20412023101099@science.zu.edu.eg>
2025-05-31 11:02:49 +0300

    Add --help command + Improve Input handling

--- include/Task.hpp
+++ include/Task.hpp
@@ -7,7 +7,7 @@ private:
     std::string m_name;
     int m_status;
 public:
-    Task(const std::string& name, int status);
+    Task(const std::string& name, int status = 0);
 
     const std::string& getName() const;
     int getStatus() const;
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <string_view>
 
 int main(int argc, char* argv[])
 {
@@ -11,32 +12,37 @@ int main(int argc, char* argv[])
 
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <command> [arguments]\n";
-        std::cout << "Commands:\n";
-        std::cout << "  add <task_name> <status>  - Add a new task\n";
-        std::cout << "  show                      - Show all tasks\n";
+        std::cerr << "No command specified. Type --help for info" << std::endl;
         return 1;
     }
 
-    if (std::string(argv[1]) == "add")
+    std::string_view command = argv[1];
+
+    if (command == "add")
     {
-        if (argc < 4)
-        {
-            std::cout << "Usage: " << argv[0] << " add <task_name> <status>\n";
-            return 1;
-        }
         const std::string& taskName = (std::string)argv[2];
-        int taskStatus = std::stoi(argv[3]);
+        int taskStatus;
+        if (argc > 3)
+            taskStatus = std::stoi(argv[3]);
 
         TO_DO.addTask(Task(taskName, taskStatus));
     }
-    else if (std::string(argv[1]) == "show")
+    else if (command == "show")
     {
         std::vector<Task*> Tasks = TO_DO.giveAllTasks();
         for (auto& task : Tasks)
         {
             std::cout << "Task: " << task->getName() << ", " << task->getStatus() << "\n";
         }
+    }
+    else if (command == "--help")
+    {
+        std::cout << "Usage: " << argv[0] << " <command> [arguments]\n";
+        std::cout << "Commands:\n";
+        std::cout << "  add <task_name> <status>  - Add a new task\n";
+        std::cout << "  show                      - Show all tasks\n";
+        std::cout << "  --help                      - Show help menu\n";
+        return 0;
     }
     else
     {
